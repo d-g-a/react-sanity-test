f7366385ea473f56e710e9f39a2166838cf2bcff
Diego Guerrero Alexander <diegoguerreroalexander@gmail.com>
2020-11-06 16:49:26 -0600

    added style-components

--- src/components/AllPosts.js
+++ src/components/AllPosts.js
@@ -1,6 +1,52 @@
 import React, { useState, useEffect} from 'react'
 import sanityClient from "../client"
 import {Link} from "react-router-dom"
+import styled from "styled-components"
+
+const AllPostsStyled = styled.div`
+
+background-color: black;
+color: white;
+width: 100vw;
+height: 100vh;
+margin: 0;
+padding: 80px;
+
+.all-cards{
+    display: flex;
+    flex-direction: row;
+}
+
+.card{
+    border: 1px solid white;
+    margin: 20px 40px 0 0;
+    text-align: center;
+}
+
+.card-image{
+    width: 200px;
+}
+
+.card-title{
+    text-transform: uppercase;
+    
+}
+
+.image-container{
+    width: 100px;
+}
+
+.card-image{
+    width: 250px;
+    height: 250px;
+    object-fit: cover;
+}
+
+a{
+    color: white;
+    text-decoration: none
+}
+`
 
 function AllPosts() {
     const [allPostData, setAllPostData] = useState(null)
@@ -23,21 +69,23 @@ function AllPosts() {
     },[])
 
     return (
-        <div>
-            <h2>Blog Posts</h2>
-            <h3>Welcome to my blog page</h3>
-            <div>
+        <AllPostsStyled>
+            <h2>STEROELAB</h2>
+            <div className="all-cards">
                 {allPostData &&
                 allPostData.map((post,index)=> (
-                    <Link to={'/'+post.slug.current} key={post.slug.current}>
-                        <span key={index}>
-                           <img src={post.mainImage.asset.url} alt="mainimage"/> 
+                    <div className="card">
+                         <Link to={'/'+post.slug.current} key={post.slug.current}>
+                        <span key={index} className="image-container">
+                           <img src={post.mainImage.asset.url} alt="mainimage" className="card-image"/> 
                         </span>
-                    </Link>
+                        <p className="card-title">{post.title}</p>
+                        </Link>
+                    </div>
                 ))}
             </div>
 
-        </div>
+        </AllPostsStyled>
     )
 }
 
